95cfd7e8e797b21aed0fabf283fadf6dc7f9bd4a
thinkinghs <45984317+thinkinghs@users.noreply.github.com>
2019-01-09 11:34:21 +0900

    Update joinfunction.py

--- joinfunction.py
+++ joinfunction.py
@@ -1,3 +1,4 @@
+# s로 string을 "abcde" 받으면 "A-Bb-Ccc-Dddd-Eeeee" 로 return 해줌
 def accum(s):
     # comprehensive for문이 list로 return 해주고, join 함수가 list 사이 사이에 '-'를 삽입해줌 
     return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
